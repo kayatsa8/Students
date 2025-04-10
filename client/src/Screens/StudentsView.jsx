1b8d5afcbde17cde4bed401f8f0ee0abd3e1e16b
kayatsa <kayatsa8@gmail.com>
2025-04-10 15:50:05 +0300

    client/src/Screens/StudentsView: including StudentTable (with demo data)

--- client/src/Screens/StudentsView.jsx
+++ client/src/Screens/StudentsView.jsx
@@ -1,7 +1,33 @@
+import StudentTable from "../Components/StudentTable";
+
 const StudentsView = () => {
+    const students = [
+        {
+            id: 1,
+            firstName: "Frodo",
+            lastName: "Baggins",
+            email: "bagginsf@shiremail.com",
+            gpa: 80
+        },
+        {
+            id: 2,
+            firstName: "Frodo",
+            lastName: "Baggins",
+            email: "bagginsf@shiremail.com",
+            gpa: 80
+        },
+        {
+            id: 3,
+            firstName: "Frodo",
+            lastName: "Baggins",
+            email: "bagginsf@shiremail.com",
+            gpa: 80
+        }
+    ];
+
     return (
         <div>
-            students
+            <StudentTable students={students}/>
         </div>
     );
 }
