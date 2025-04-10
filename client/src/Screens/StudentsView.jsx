23c1a4bef41a2ac7426326d0110431885accfac6
kayatsa <kayatsa8@gmail.com>
2025-04-10 15:45:15 +0300

    client/src/Screens/StudentsView: Students -> StudentsView

--- client/src/Screens/StudentsView.jsx
+++ client/src/Screens/StudentsView.jsx
@@ -1,4 +1,4 @@
-const Students = () => {
+const StudentsView = () => {
     return (
         <div>
             students
@@ -6,4 +6,4 @@ const Students = () => {
     );
 }
  
-export default Students;
+export default StudentsView;
