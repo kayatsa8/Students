47bca94ad1d34714b821de4490505763633eba07
kayatsa <kayatsa8@gmail.com>
2025-04-10 15:44:30 +0300

    client/src/Components/StudentTable: adding student table

--- client/src/Components/StudentTable.jsx
+++ client/src/Components/StudentTable.jsx
@@ -1,8 +1,18 @@
-const StudentTable = () => {
+import StudentRow from "./StudentRow";
+
+const StudentTable = ({students}) => {
     return (
-        <div>
-            StudentTable
-        </div>
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>GPA</th>
+            </tr>
+
+            {students.map((student) => <StudentRow student={student}/>)}
+        </table>
     );
 }
  
