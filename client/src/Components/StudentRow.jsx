6e4e6f3b0a2f1b8a3ca9ac3f3eac10cc892aa214
kayatsa <kayatsa8@gmail.com>
2025-04-10 15:41:45 +0300

    client/src/Components/StudentRow: student row component

---
+++ client/src/Components/StudentRow.jsx
@@ -0,0 +1,13 @@
+const StudentRow = ({student}) => {
+    return (
+        <tr>
+            <td>{student.id}</td>
+            <td>{student.firstName}</td>
+            <td>{student.lastName}</td>
+            <td>{student.email}</td>
+            <td>{student.gpa}</td>
+        </tr>
+    );
+}
+ 
+export default StudentRow;
