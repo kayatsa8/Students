65e1dac01b246fe87547f316c36582696cc440a3
kayatsa <kayatsa8@gmail.com>
2025-04-10 10:47:55 +0300

    Server/students/../model/Student: adding update() to Student.

--- Server/students/src/main/java/com/students/students/model/Student.java
+++ Server/students/src/main/java/com/students/students/model/Student.java
@@ -18,4 +18,12 @@ public class Student {
     private String email;
     private Department department;
     private Float GPA;
+
+    public void update(StudentUpdate updated){
+        this.firstName = updated.getFirstName() == null ? this.firstName : updated.getFirstName();
+        this.lastName = updated.getLastName() == null ? this.lastName : updated.getLastName();
+        this.email = updated.getEmail() == null ? this.email : updated.getEmail();
+        this.department = updated.getDepartment() == null ? this.department : updated.getDepartment();
+        this.GPA = updated.getGPA() == null ? this.GPA : updated.getGPA();
+    }
 }
