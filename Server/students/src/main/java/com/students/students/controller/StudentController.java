3c6f63e09edb74ecbf581394a6fc1161c4a238b3
kayatsa <kayatsa8@gmail.com>
2025-04-10 11:15:08 +0300

    Server/students/../controller/StudentController: constructor & getAll()

--- Server/students/src/main/java/com/students/students/controller/StudentController.java
+++ Server/students/src/main/java/com/students/students/controller/StudentController.java
@@ -1,6 +1,31 @@
 package com.students.students.controller;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.students.students.model.Student;
+import com.students.students.service.StudentService;
+
+@RestController
+@RequestMapping("/api/students")
 public class StudentController {
-    
+    private StudentService service;
+
+
+    @Autowired
+    public StudentController(StudentService service){
+        this.service = service;
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/get/all")
+    public List<Student> getAll(){
+        return service.getAll();
+    }
 }
