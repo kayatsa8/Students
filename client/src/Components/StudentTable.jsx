334cb7e530080830c996373a4b1ccecba625f169
kayatsa <kayatsa8@gmail.com>
2025-04-10 15:52:31 +0300

    client/src/Components/StudentTable: adding key property + using tbody

--- client/src/Components/StudentTable.jsx
+++ client/src/Components/StudentTable.jsx
@@ -3,15 +3,17 @@ import StudentRow from "./StudentRow";
 const StudentTable = ({students}) => {
     return (
         <table>
-            <tr>
-                <th>Id</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>GPA</th>
-            </tr>
+            <tbody>
+                <tr>
+                    <th>Id</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>GPA</th>
+                </tr>
 
-            {students.map((student) => <StudentRow student={student}/>)}
+                {students.map((student) => <StudentRow key={student.id} student={student}/>)}
+            </tbody>
         </table>
     );
 }
