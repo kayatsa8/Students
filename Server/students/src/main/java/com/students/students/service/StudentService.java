8690fad6eb700d1b137171ca0d16edc91a422f04
kayatsa <kayatsa8@gmail.com>
2025-04-10 10:50:40 +0300

    Server/students/../service/StudentService: adding updateStudent()

--- Server/students/src/main/java/com/students/students/service/StudentService.java
+++ Server/students/src/main/java/com/students/students/service/StudentService.java
@@ -1,11 +1,13 @@
 package com.students.students.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.students.students.model.Student;
+import com.students.students.model.StudentUpdate;
 import com.students.students.repository.StudentRepository;
 
 @Service
@@ -25,6 +27,23 @@ public class StudentService {
     }
 
     public void addStudent(Student student){
+        repo.save(student);
+    }
+
+    public void updateStudent(StudentUpdate updated) throws Exception{
+        if(updated.getId() == null){
+            throw new Exception("given id is null");
+        }
+
+        Optional<Student> oStudent = repo.findById(updated.getId());
+
+        if(oStudent.isEmpty()){
+            throw new Exception("a student with id " + updated.getId() + " does not exist");
+        }
+
+        Student student = oStudent.get();
+        student.update(updated);
+
         repo.save(student);
     }
 
