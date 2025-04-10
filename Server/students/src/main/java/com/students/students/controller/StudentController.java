f06cf06b2445ddb233fda46c1499b8ab4a86a87d
kayatsa <kayatsa8@gmail.com>
2025-04-10 11:19:14 +0300

    Server/students/../controller/StudentController: adding addStudent()

--- Server/students/src/main/java/com/students/students/controller/StudentController.java
+++ Server/students/src/main/java/com/students/students/controller/StudentController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.students.students.model.Student;
 import com.students.students.service.StudentService;
@@ -27,5 +31,16 @@ public class StudentController {
     @GetMapping("/get/all")
     public List<Student> getAll(){
         return service.getAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/add")
+    public void addStudent(@RequestBody Student student){
+        try{
+            service.addStudent(student);
+        }
+        catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 }
