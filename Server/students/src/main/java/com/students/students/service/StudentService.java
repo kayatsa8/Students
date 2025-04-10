dea7d0966d9101fb08763145845acf105612e5a4
kayatsa <kayatsa8@gmail.com>
2025-04-10 10:31:22 +0300

    Server/students/../service/StudentService: constructor & getAll()

--- Server/students/src/main/java/com/students/students/service/StudentService.java
+++ Server/students/src/main/java/com/students/students/service/StudentService.java
@@ -1,6 +1,29 @@
 package com.students.students.service;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.students.students.model.Student;
+import com.students.students.repository.StudentRepository;
+
+@Service
 public class StudentService {
-    
+    private StudentRepository repo;
+
+
+    @Autowired
+    public StudentService(StudentRepository repository){
+        this.repo = repository;
+    }
+
+    public List<Student> getAll(){
+        List<Student> students = repo.findAll();
+
+        return students;
+    }
+
+
+
 }
