a3f171e9cb30476e80bfc5e7d12db54ae729e8be
kayatsa <kayatsa8@gmail.com>
2025-04-11 09:42:20 +0300

    client/src/Screens/AddStudent: department is choosed with select

--- client/src/Screens/AddStudent.jsx
+++ client/src/Screens/AddStudent.jsx
@@ -1,18 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AddStudent = () => {
     const navigate = useNavigate();
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
-    const [department, setDepartment] = useState("");
-    const [gpa, setGpa] = useState(null);
+    const [department, setDepartment] = useState("SOFTWARE_ENGINEERING");
+    const [gpa, setGpa] = useState(0);
+    const [departments, setDepartments] = useState({});
 
 
     const handleBackClick = () => {
         navigate("/");
     };
+
+    const handleSubmit = () => {
+
+    };
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/students/departments")
+                .then((response) => {
+                    if(response.status !== 200){
+                        throw new Error(response.statusText);
+                    }
+
+                    return response.data;
+                })
+                .then((data) => {
+                    setDepartments(() => data);
+                })
+                .catch((err) => {
+                    setError(() => err);
+                });
+    }, []);
 
 
 
@@ -22,7 +45,7 @@ const AddStudent = () => {
 
             <h2>Add Student</h2>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>First Name: </label>
                 <input
                     required
@@ -50,15 +73,12 @@ const AddStudent = () => {
                     onChange={(e) => setEmail(() => e.target.value)}
                 />
 
-                {/* TODO: make select */}
                 <label>Department: </label>
-                <input
-                    required
-                    type="text"
-                    value={department}
-                    placeholder=""
-                    onChange={(e) => setEmail(() => e.target.value)}
-                />
+                <select value={department} onChange={(e) => setDepartment(() => e.target.value)}>
+                    {Object.entries(departments).map(([dep, depName]) => 
+                        <option key={dep} value={dep}>{depName}</option>
+                    )}
+                </select>
 
                 <label>GPA: </label>
                 <input
@@ -68,6 +88,8 @@ const AddStudent = () => {
                     placeholder={82.13}
                     onChange={(e) => setGpa(() => e.target.value)}
                 />
+
+                <button>Add</button>
             </form>
         </div>
     );
